594bc11b03830d865a743f419a9050b5f4577b04
Daniel Grey <64065022+weazzyefff@users.noreply.github.com>
2020-05-05 19:53:43 +1200

    Just finished adding search bar. Still no functionality yet.

--- myapp/danskeapp/views.py
+++ myapp/danskeapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return render(request, 'my_app/new_search.html')
+    search = request.POST.get('search')
+    print(search)
+    # Dictionary for something
+    stuff_for_frontend = {
+        'search': search
+    }
+    return render(request, 'myapp/new_search.html', stuff_for_frontend)
