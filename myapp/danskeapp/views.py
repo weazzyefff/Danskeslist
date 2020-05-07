21ac46584451494eb3f8f6c39a9e936869bf9f00
Daniel Grey <64065022+weazzyefff@users.noreply.github.com>
2020-05-07 21:35:44 +1200

    Pulls images now. Still aren't uniform size though.

--- myapp/danskeapp/views.py
+++ myapp/danskeapp/views.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://auckland.craigslist.org/search/sss?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -34,7 +35,16 @@ def new_search(request):
         else:
             post_price = 'N/A'
 
-        final_postings.append((post_title, post_url, post_price))
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+
 
     # print(post_title)  # Commenting these out for now but they
                          # came in really helpful when testing FYI
